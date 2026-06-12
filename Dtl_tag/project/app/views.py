7475a54e29989547c57f49a1a1ac344cfb01fb6a
Neeraj Kumar <nkurmbanshi@yahoo.com>
2026-06-12 06:14:18 +0530

    12-06-2026

--- Dtl_tag/project/app/views.py
+++ Dtl_tag/project/app/views.py
@@ -5,5 +5,7 @@ def landing(req):
     data = {"name":"Neeraj","city":"Bhopal"}
     # return render(req,'landing.html')
     # return render(req,'landing.html',{"xyz":data})
+    # return render(req,'landing.html',{"pqr":[10,20,30,40,50]}) # for with list of object
     return render(req,'landing.html',{"pqr":"Hello World"})
+    
 
