8866877c372837ad756a42169e3ff95f25bb7e90
Neeraj Kumar <nkurmbanshi@yahoo.com>
2026-06-15 06:16:02 +0530

    15-06-2026

--- Dtl_tag/project/app/views.py
+++ Dtl_tag/project/app/views.py
@@ -6,6 +6,9 @@ def landing(req):
     # return render(req,'landing.html')
     # return render(req,'landing.html',{"xyz":data})
     # return render(req,'landing.html',{"pqr":[10,20,30,40,50]}) # for with list of object
+    # return render(req,'landing.html',{"pqr":"Hello World"})
+
+    # forloop.counter
     return render(req,'landing.html',{"pqr":"Hello World"})
     
 
